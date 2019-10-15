538534cb2e71fea10402e547754781fdcaa2822e
troyyxk <xingkunyin.hotmail.com>
2019-10-14 21:54:21 -0400

    Make print bitmap a helper function.

--- readimage.c
+++ readimage.c
@@ -11,7 +11,18 @@
 
 #include "a1fs.h"
 #include "map.h"
-#include "util.h"
+
+/** Print Bitmap */
+int print_bitmap(unsigned char *bitmap, int size){
+    for (int bit = 0; bit < size; bit++)
+    {
+        printf("%d", (bitmap[bit] & (1 << bit)) > 0);
+        if ((bit + 1) % 5 == 0)
+        {
+            printf(" ");
+        }
+    }
+}
 
 // Pointer to the 0th byte of the disk
 unsigned char *disk;
