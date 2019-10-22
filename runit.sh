25202db4690e752d2d54df338b22e74e0fae3176
shouko-inori <frank.gong.1006@hotmail.com>
2019-10-21 22:15:39 -0400

    Completion of the shell code

--- runit.sh
+++ runit.sh
@@ -5,23 +5,24 @@ truncate -s 64M img
 mkfs.a1fs -i 100 img
 a1fs img /tmp/mnt99
 
-cd /tmp/mnt99 || exit
-mkdir dir_1
-cd dir_1 || exit
-mkdir dir_1_1
-cd ..
-rmdir dir_1/dir_1_1
+mkdir /tmp/mnt99/dir_1
+mkdir /tmp/mnt99/dir_1/dir_1_1
+rmdir /tmp/mnt99/dir_1/dir_1_1
 
-touch f1
-stat f1
-touch dir_1/f1
-unlink dir_1/f1
+touch /tmp/mnt99/f1
+stat /tmp/mnt99/f1
+touch /tmp/mnt99/dir_1/f1
+unlink /tmp/mnt99/dir_1/f1
 
-echo "testflight" >> f1
-cat f1
+echo "testflight" >> /tmp/mnt99/f1
+cat /tmp/mnt99/f1
 
-stat f1
-truncate -s 8192 f1
-stat f1
-truncate -s 4 f1
-stat f1
+stat /tmp/mnt99/f1
+truncate -s 8192 /tmp/mnt99/f1
+stat /tmp/mnt99/f1
+truncate -s 4 /tmp/mnt99/f1
+stat /tmp/mnt99/f1
+
+fusermount -u /tmp/mnt99/
+a1fs img /tmp/mnt99
+stat /tmp/mnt99/f1
